09ec2f79301f2f6662dcba60550e3654ed4ccec2
Ruben Villagrana <RubenVillagrana35@gmail.com>
2022-11-16 21:06:02 -0800

    Lecture activities

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let myGrades = [ 95, 100, 76, 100, 85 ];
+console.log(myGrades[0]); // 95
+console.log(myGrades[4]); // 85
+console.log(myGrades.length); // 5
 
+
+myGrades.push(200);
+console.log(myGrades);//
+
+myGrades.push(1500);
+console.log(myGrades);//
+
+myGrades.push(900);
+console.log(myGrades);//
+
+myGrades.push(12);
+console.log(myGrades);//
+
+myGrades.push(69);
+console.log(myGrades);//
+
+
+console.log(myGrades.pop());// 69
+
+console.log(myGrades.pop());// 12
+
+console.log(myGrades.pop());// 900
+
+
+let myString = "That a Girl!";
+console.log ()
